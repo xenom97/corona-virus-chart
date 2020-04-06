1c36154bab00dfe241d0ecac7037176877ce366f
ahmad purnomo <purnomo.ahmad97@gmail.com>
2020-04-06 08:01:48 +0700

    done

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) changeableUrl = `${url}/countries/${country}`;
+
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+
+    const modifiedData = { confirmed, recovered, deaths, lastUpdate };
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((item) => ({
+      confirmed: item.confirmed.total,
+      deaths: item.deaths.total,
+      date: item.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((item) => item.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
